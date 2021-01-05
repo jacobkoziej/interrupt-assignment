ffde417d431157d8b911f2eb1346c8dcfcca7401
Jacob Koziej <jacobkoziej@gmail.com>
2021-01-05 10:21:44 -0500

    Add task-1.ino

---
+++ task-1.ino
@@ -0,0 +1,26 @@
+/*
+ * Jacob Koziej
+ * Interrupt Assignment (Task 1)
+ * 2020-01-05
+ */
+
+
+volatile uint8_t state = 0;
+
+
+void setup()
+{
+	pinMode(2,  INPUT);
+	pinMode(7, OUTPUT);
+	attachInterrupt(digitalPinToInterrupt(2), toggle, CHANGE);
+}
+
+void loop()
+{
+}
+
+void toggle()
+{
+	state = !state;
+	digitalWrite(7, state);
+}
